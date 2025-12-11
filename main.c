3d356d3eada047931e01bf01a8a17744b7fb9235
Maksims <112962074+MaksimsTurs@users.noreply.github.com>
2025-12-11 13:45:41 +0100

    Update build command in main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "ccanvas.h"
 
-// clang -Wall -Wextra -O2 main.c ccanvas.c -o main
+// clang -Wall -Wextra -O2 main.c ./src/ccanvas.c -o main
 
 #define CANVAS_WIDTH  100
 #define CANVAS_HEIGHT 100
